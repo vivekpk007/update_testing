ddfcae000f4b33cdb27b1faa07df4e496e4aa2c7
vivekpk007 <vivekpk007@gmail.com>
2016-07-20 10:51:30 +0530

    first code blink

---
+++ blink.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO ## Import GPIO library
+import time ## Import 'time' library. Allows us to use 'sleep'
+
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+
+numTimes
+
+def Blink(numTimes,speed):
+    for i in range(0,numTimes):## Run loop numTimes
+        print "Iteration " + str(i+1)## Print current loop
+        GPIO.output(7,True)## Switch on pin 7
+        time.sleep(speed)## Wait
+        GPIO.output(7,False)## Switch off pin 7
+        time.sleep(speed)## Wait
+    print "Done" ## When loop is complete, print "Done"
+    GPIO.cleanup()
+iterations = raw_input("Enter total number of times to blink: ")
+speed = raw_input("Enter length of each blink(seconds): ")
+Blink(int(iterations),float(speed))
